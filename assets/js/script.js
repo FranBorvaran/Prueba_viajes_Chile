4ec8c03e5c36eb1605fb366cae9ed72980b7e511
unknown <f.borvaran@gmail.com>
2021-07-11 22:33:00 -0400

    Estilos css y funciones JS

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+
+    //ocultar texto de card
+    $(".card-title").click(function () {
+        $(".card-text").css("display", "none");
+        
+
+})   
